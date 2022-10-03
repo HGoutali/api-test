827f2a2e76f70a074160f0184ab0166380e7f6d4
hgoutali <hh88remp@yahoo.fr>
2022-10-03 14:14:20 +0200

    mod pwd

--- app/config/dbpostgre.config.js
+++ app/config/dbpostgre.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-  HOST: "ec2-54-228-125-183.eu-west-1.compute.amazonaws.com",
-  USER: "xvxttkrnodwxyd",
+  HOST: "ec2-34-252-35-249.eu-west-1.compute.amazonaws.com",
+  USER: "dsqksqybiecasp",
   PASSWORD: "14dc3dd5a841f76e6698872cbb806030294d661af78b82440133d2417f1607fa",
-  DB: "d3e5upumbpjia0",
+  DB: "dfc8nvqq60i3fb",
   dialect: "postgres",
   port:5432, 
   dialectOptions: {
