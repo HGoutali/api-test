90abcc35cb84b6403dfc6f4a779e74bf609859cc
hgoutali <hh88remp@yahoo.fr>
2022-10-20 16:06:09 +0200

    use config.js

--- app/config/config.js
+++ app/config/config.js
@@ -23,20 +23,20 @@ const dev = {
 
 const test = {
  app: {
-   port: parseInt(process.env.DEV_APP_PORT) || 5000
+   port: parseInt(process.env.PORT) || 5000
  },
  db: {
-  host: process.env.DEV_DB_HOST || 'ec2-34-252-35-249.eu-west-1.compute.amazonaws.com',
-  port: parseInt(process.env.PORT) || 5432,
-  user: process.env.DEV_DB_USER || 'dsqksqybiecasp',
-  password: process.env.DEV_DB_PASSWORD || '14dc3dd5a841f76e6698872cbb806030294d661af78b82440133d2417f1607fa',
-  name: process.env.DEV_DB_NAME || 'dfc8nvqq60i3fb',
-  dialect: process.env.DEV_DB_DIALECT || 'postgres',
+  host: process.env.TEST_DB_HOST || 'ec2-34-252-35-249.eu-west-1.compute.amazonaws.com',
+  port: parseInt(process.env.TEST_DB_PORT) || 5432,
+  user: process.env.TEST_DB_USER || 'dsqksqybiecasp',
+  password: process.env.TEST_DB_PASSWORD || '14dc3dd5a841f76e6698872cbb806030294d661af78b82440133d2417f1607fa',
+  name: process.env.TEST_DB_NAME || 'dfc8nvqq60i3fb',
+  dialect: process.env.TEST_DB_DIALECT || 'postgres',
   pool: {
-    max: parseInt(process.env.DEV_DB_max) || 5,
-    min: parseInt(process.env.DEV_DB_min) || 0,
-    acquire: parseInt(process.env.DEV_DB_acquire) || 30000,
-    idle: parseInt(process.env.DEV_DB_idle) || 10000
+    max: parseInt(process.env.TEST_DB_max) || 5,
+    min: parseInt(process.env.TEST_DB_min) || 0,
+    acquire: parseInt(process.env.TEST_DB_acquire) || 30000,
+    idle: parseInt(process.env.TEST_DB_idle) || 10000
   }
  }
 };
